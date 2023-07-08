dfd2976c5b143aa06d1c6d8a830604bf288538d8
Raekwon Williams <Raekwill15@gmail.com>
2023-07-08 02:24:52 -0400

    Components created and added to app.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,1 +1,16 @@
+import React, {useContext} from 'react';
+import { AppContext } from '../context/AppContext';
 
+const ExpenseTotal = () => {
+    const { expenses } = useContext(AppContext);
+    const totalspent = expenses.reduce((total,item) => {
+        return (total += item.cost);
+    }, 0)
+    return (
+        <div className='alert alert-primary'>
+            <span>Spent so far: £{totalspent}</span>
+        </div>
+    )
+}
+
+export default ExpenseTotal;
